c6d15217e391d48cc05c0366ed34519bed856a19
Murali <muralig436@gmail.com>
2023-10-17 11:49:10 +0530

    now the function returns an resultant arraylist

--- java/Recursion/Generate.java
+++ java/Recursion/Generate.java
@@ -1,18 +1,30 @@
 package Recursion;
 
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.ArrayList;
+
 public class Generate {
     public static void main(String[] args) {
-        generate("","abc");
+        ArrayList<String>res=generate("","abc");
+
+        System.out.println(res);
     }
-    private static void generate(String output,String input){
+    private static ArrayList<String> generate(String output,String input){
+
+
 
         if (input.length()==0){
-            System.out.println(output);
-            return;
+            ArrayList<String>list=new ArrayList<>();
+            list.add(output);
+            return list;
         }
 
-        generate(output+input.charAt(0),input.substring(1));
+        ArrayList<String>left=generate(output+input.charAt(0),input.substring(1));
 
-        generate(output,input.substring(1));
+        ArrayList<String>right=generate(output,input.substring(1));
+
+        left.addAll(right);
+
+        return left;
     }
 }
