7f6a6ac138b47543f81dd8031faa141b0538e3b7
Murali <muralig436@gmail.com>
2023-10-17 11:24:50 +0530

    modified Generate.java removed parameter index from the function

--- java/Recursion/Generate.java
+++ java/Recursion/Generate.java
@@ -4,15 +4,15 @@ public class Generate {
     public static void main(String[] args) {
         generate("","abc",0);
     }
-    private static void generate(String output,String input,int index){
+    private static void generate(String output,String input){
 
         if (input.length()==0){
             System.out.println(output);
             return;
         }
 
-        generate(output+input.charAt(index),input.substring(index+1),index);
+        generate(output+input.charAt(1),input.substring(1));
 
-        generate(output,input.substring(index+1),index);
+        generate(output,input.substring(1));
     }
 }
